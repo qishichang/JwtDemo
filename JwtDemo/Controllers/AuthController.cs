374228dbb1f4da75a6cc9f11c7c921ce38ec44b0
qishichang <qishichang@live.com>
2018-10-16 16:23:32 +1300

    Add project files.

---
+++ JwtDemo/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("token")]
+        public ActionResult GetToken()
+        {
+            //security key
+            var securityKey = "this_is_our_supper_long_security_key_for_token_validation_project_2018_10_16$semsk.in";
+            //symmetric security key
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+            //signing credentials
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            // add claims
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+            claims.Add(new Claim("Our_Custom_Claim", "Our custom value"));
+
+
+            //create token
+            var token = new JwtSecurityToken(
+                issuer: "smesk.in",
+                audience: "readers",
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: signingCredentials,
+                claims: claims
+                );
+            //return token
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+        }
+    }
+}
